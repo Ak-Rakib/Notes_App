1599dc030e9aca99db838560c67129f3d41158c8
Ak-Rakib <rkrakibsaimon@gmail.com>
2025-01-20 10:29:25 +0600

    coding other function and this project are done

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,11 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
 function updateStorage() {
     localStorage.setItem("notes", notesContainer.innerHTML);
 }
@@ -21,5 +26,21 @@ notesContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
         updateStorage();
+    }
+    else if (e.target.tagName === "P") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+                updateStorage();
+            }
+        });
+    }
+});
+
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 });
